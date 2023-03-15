f8855114a222d8b088d904188e98c8948d46102c
your name <your-email@foo.com>
2023-03-15 14:18:13 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction (spy) {
+    return spy ()
+}
+
+returnsANamedFunction;
+ function returnsANamedFunction() {
+    return function Derrick (){
+    }
+ }
+
+function returnsAnAnonymousFunction() {
+    return () =>{
+
+    }
+}
